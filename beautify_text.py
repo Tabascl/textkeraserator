245e5462f130780d615c9edf804a006a02870857
Tabascl <simonwilde93@gmail.com>
2017-12-12 17:15:46 +0100

    Add beautify_text

---
+++ beautify_text.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+
+def clean_text(filename):
+    text = open(filename, 'r').read()
+
+    soup = BeautifulSoup(text)
+    hum_text = soup.get_text()
+
+    clean_text = ''.join([i if ord(i) < 128 else '' for i in text])
+
+    with open('clean_posts.txt', 'w') as f:
+        f.write(clean_text)
