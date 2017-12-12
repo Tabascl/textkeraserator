ee6dc8faa0f42d25d1e7a48434c3ce6db0db4cba
Tabascl <simonwilde93@gmail.com>
2017-12-12 18:14:41 +0100

    Improve text beautifying

--- beautify_text.py
+++ beautify_text.py
@@ -1,15 +1,27 @@
 import warnings
+import re
+
 from bs4 import BeautifulSoup
 
 warnings.filterwarnings("ignore", category=UserWarning, module='bs4')
 
+
 def clean_text(filename):
     text = open(filename, 'r').read()
 
+    text = re.sub('<br>', '\n', text)
+    text = re.sub('\n+', '\n', text)
+
     soup = BeautifulSoup(text)
-    hum_text = soup.get_text()
+    text = soup.get_text()
+
+    text = re.sub('>>[0-9]+', '', text)
 
     clean_text = ''.join([i if ord(i) < 128 else '' for i in text])
 
     with open('clean_posts.txt', 'w') as f:
         f.write(clean_text)
+
+
+if __name__ == '__main__':
+    clean_text('raw_posts.txt')
